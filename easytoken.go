13bdccfd23c83ff90f9fb92ac90d1613b05edc3e
Dollarkillerx <dollarkiller@vip.qq.com>
2019-08-09 11:07:28 +0800

    add

--- easytoken.go
+++ easytoken.go
@@ -26,7 +26,8 @@ type easyJwtUtils struct {
 }
 
 var (
-	EasyJwtUtils *easyJwtUtils
+	easyJwtUtil *easyJwtUtils
+	db_com *sync.Map
 )
 
 func init() {
@@ -40,7 +41,10 @@ func init() {
 		priKey: priKey,
 		pubKey: pubKey,
 	}
-	EasyJwtUtils = &data
+	easyJwtUtil = &data
+
+	// 初始化 通用存储
+	db_com = &sync.Map{}
 }
 
 // 生成tooken
@@ -56,7 +60,7 @@ func EasyJwtGeneraToken(data *EasyJwtPayload,hour int) (string,error) {
 	}
 	payloadEnco := Base64Encode(payloadJson)
 	// 签名
-	signature, e := RsaSignSimple(payloadEnco, EasyJwtUtils.priKey) // 签名
+	signature, e := RsaSignSimple(payloadEnco, easyJwtUtil.priKey) // 签名
 	if e != nil {
 		log.Println(e.Error())
 		return "", e
@@ -64,7 +68,9 @@ func EasyJwtGeneraToken(data *EasyJwtPayload,hour int) (string,error) {
 
 	jwt := payloadEnco + "." + signature
 	// 存入内存中
-	EasyJwtUtils.data.Store(jwt,data)
+	easyJwtUtil.data.Store(jwt,data)
+	// 将用户id 和 jwt 存入到通用存储中
+	db_com.Store(data.Iss,jwt)
 
 	return jwt,nil
 }
@@ -72,7 +78,7 @@ func EasyJwtGeneraToken(data *EasyJwtPayload,hour int) (string,error) {
 // 验证签名
 // jwt
 func EasyJwtVerification(jwt string) error {
-	value, ok := EasyJwtUtils.data.Load(jwt)
+	value, ok := easyJwtUtil.data.Load(jwt)
 	if !ok {
 		return errors.New("not data")
 	}
@@ -82,8 +88,25 @@ func EasyJwtVerification(jwt string) error {
 	// 如果过期
 	if payload.Exp < time {
 		// 在内存中删除 并返回错误
-		EasyJwtUtils.data.Delete(jwt)
+		easyJwtUtil.data.Delete(jwt)
+		// 删除 通用存储中的用户信息
+		db_com.Delete(payload.Iss)
 		return errors.New("time out")
+	}
+	return nil
+}
+
+// 验证用户是否已经登录
+func EasyJwtCheckUserLogin(email string) error {
+	value, ok := db_com.Load(email)
+	if !ok {
+		return errors.New("not data")
+	}
+	token := value.(string)
+	// 验证token 是否有效
+	verification := EasyJwtVerification(token)
+	if verification != nil {
+		return errors.New("not login")
 	}
 	return nil
 }
